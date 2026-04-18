0041a59022c31c999be95a26aea039be6dc5f9a5
Ashab Uddin <ashabuddin47@gmail.com>
2026-04-18 18:35:16 +0600

    solve a basic problem

---
+++ Basic problem/sum_of_odd_even_number.py
@@ -0,0 +1,11 @@
+numbers =list(map(int, input("Enter the numbers:").split()))
+even_numbers = 0;
+odd_numbers = 0;
+for num in numbers:
+    if num%2==0:
+        even_numbers = even_numbers+num
+    else:
+        odd_numbers= odd_numbers+num
+
+print("Sum of Even number: ",even_numbers)
+print("Sum of Odd number: ",odd_numbers)
