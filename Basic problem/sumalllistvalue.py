7e13b7084037c4cd2ea0938f97abb0a66423c3d7
Ashab Uddin <ashabuddin47@gmail.com>
2026-04-18 21:29:45 +0600

    find largest value at this list

--- Basic problem/sumalllistvalue.py
+++ Basic problem/sumalllistvalue.py
@@ -1,7 +1,11 @@
 list = [10,30,40]
 sum =0
+large = list[0]
 for i in list:
     sum = sum+i
+    if large<i:
+        large = i
+
 
 print("Sum of value at this list: ",sum)
-
+print("largest value of this list: ",large)
